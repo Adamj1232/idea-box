0691fc804480a8fa7a2937c1f0c54aa803a57dfb
Adam Carpenter <adamcarpenter86@gmail.com>
2017-02-15 15:46:32 -0700

    More JS changes and mostly working search

--- idea-box.js
+++ idea-box.js
@@ -15,9 +15,9 @@ $('#submit').prop('disabled', true);
 function prepend(idea) {
   $('.idea-field').prepend(`
   <article class="entry" id="${idea.id}">
-    <h3 class="title"> ${idea.title} </h3>
+    <h3 class="title" contenteditable="true"> ${idea.title} </h3>
     <button class="delete"> Delete </button>
-    <p> ${idea.body}</p>
+    <p class="ideaEdit" contenteditable="true"> ${idea.body}</p>
     <button class="up"></button>
     <button class="down"></button>
     <p class="quality">quality: ${idea.quality}</p>
@@ -35,6 +35,22 @@ $('.submit').on('click', function(){
     clearField();
 })
 
+$('.idea-field').on('focusout', '.title', function(){
+  var editId = $(this).parent().attr('id');
+  var editTitle = $(this).closest('.title').html();
+  var updated = JSON.parse(localStorage.getItem(editId));
+  updated.title = editTitle;
+  localStorage.setItem(editId, JSON.stringify(updated));
+})
+
+$('.idea-field').on('focusout', '.ideaEdit', function(){
+  var editId = $(this).parent().attr('id');
+  var editBody = $(this).closest('.ideaEdit').html();
+  var updated = JSON.parse(localStorage.getItem(editId));
+  updated.body = editBody;
+  localStorage.setItem(editId, JSON.stringify(updated))
+})
+
 $('.idea-field').on('click', '.delete', function(){
   var rmvId = $(this).parent().attr('id');
   $(this).parent('.entry').remove();
@@ -47,6 +63,18 @@ $('input[type=text]').on('keyup', function () {
   } else {
     $('#submit').prop('disabled', true);
   }
+})
+
+
+$('#search').on('keyup', function() {
+  var searchInput = $(this).val().toLowerCase();
+  var ideaBoxes = $('.entry');
+
+  ideaBoxes.each(function(i, idea) {
+    var ideaText = $(idea).text().toLowerCase();
+    var matchedIdea = ideaText.indexOf(searchInput) !== -1;
+    $(idea).toggle(matchedIdea);
+  })
 })
 
 function disableSubmit() {
@@ -76,8 +104,4 @@ function retrieveIdea() {
   var storeBody = $('#body').val();
   var storeTitle = $('#title').val();
   var idea = new Idea (storeTitle, storeBody)
-}
-
-  this.parent.removeItem
-
 }
